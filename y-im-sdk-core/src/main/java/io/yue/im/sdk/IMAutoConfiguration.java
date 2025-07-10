7bcc60318e213163627195d8ce5008fe2f16d7f1
siehun <126447514+siehun@users.noreply.github.com>
2025-07-10 21:52:51 +0800

    配置相关

---
+++ y-im-sdk-core/src/main/java/io/yue/im/sdk/IMAutoConfiguration.java
@@ -0,0 +1,9 @@
+package io.yue.im.sdk;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.yue.im.sdk")
+public class IMAutoConfiguration {
+}
